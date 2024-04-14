9b0e1d86ba04503cae0023b4a8b9b2724d268894
kaixindelele <1985790413@qq.com>
2024-04-14 23:49:06 +0800

    自动解压压缩包和重命名md文件

--- pdf2md.py
+++ pdf2md.py
@@ -1,5 +1,7 @@
 import requests as rq
 import json
+import os
+import zipfile
 
 class PDF2MD:
     def __init__(self, api_key):
@@ -8,10 +10,13 @@ class PDF2MD:
         self.export_url = "https://api.doc2x.noedgeai.com/api/export"
 
     def convert(self, filepath, to="md"):
+        filename = os.path.splitext(os.path.basename(filepath))[0]
+        
         res = rq.post(self.url, files={"file": open(filepath, "rb")}, headers={"Authorization": "Bearer " + self.api_key}, stream=True)
 
         if res.status_code == 200:
-            with open("demo.txt", "w", encoding="utf-8") as f:
+            txt_path = filename + ".txt"
+            with open(txt_path, "w", encoding="utf-8") as f:
                 for line in res.iter_lines():
                     if len(line) > 0:
                         decoded_line = line.decode("utf-8")
@@ -22,9 +27,9 @@ class PDF2MD:
             print(uuid)
             
             if to == "md" or to == 'latex':
-                path = 'demo.' + 'zip'
+                path = filename + '.zip'
             elif to == 'docx':
-                path = 'demo.docx'
+                path = filename + '.docx'
             
             export_url = self.export_url + "?request_id=" + uuid + "&to=" + to
             res = rq.get(export_url, headers={"Authorization": "Bearer " + self.api_key})
@@ -32,7 +37,31 @@ class PDF2MD:
             if res.status_code == 200:
                 with open(path, "wb") as f:
                     f.write(res.content)
-                print("保存成功，存入：", path)
+                print("下载成功,存入:", path)
+                
+                if to == "md" or to == 'latex':
+                    zip_file = zipfile.ZipFile(path)
+                    
+                    # 创建以原始文件名命名的文件夹
+                    if not os.path.exists(filename):
+                        os.mkdir(filename)
+                    
+                    # 解压到该文件夹内
+                    for names in zip_file.namelist():
+                        zip_file.extract(names, filename)
+                    zip_file.close()
+                    
+                    # 找到解压后的md文件
+                    for file in os.listdir(filename):
+                        if file.endswith(".md"):
+                            extracted_md = os.path.join(filename, file)
+                            break
+                    
+                    # 重命名md文件
+                    new_md_name = os.path.join(filename, filename+'.md')
+                    os.rename(extracted_md, new_md_name)
+                    print("解压并重命名md文件为:", new_md_name)
+                
             else:
                 print(format("[ERROR] status code: %d, body: %s" % (res.status_code, res.text)))
         else:
@@ -41,7 +70,7 @@ class PDF2MD:
 
 
 def main():
-    api_key = "sk-xxxx"
+    api_key = "sk-xxx"
     filepath = r"test.pdf"
     converter = PDF2MD(api_key)
     converter.convert(filepath, to="md")
